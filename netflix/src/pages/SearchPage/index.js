de50c486e8da7fd1db280f3faee72f15cfc29a7b
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2023-03-27 13:04:01 +0900

    feat: 검색 1차 구현

--- netflix/src/pages/SearchPage/index.js
+++ netflix/src/pages/SearchPage/index.js
@@ -1,11 +1,71 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import api from '../../api/api';
+import "./SearchPage.css";
 const SearchPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const [searchResult, setSearchResult] = useState([]);
+
+    const useQuery = () => {
+        return new URLSearchParams(useLocation().search);
+    }
+    let query = useQuery();
+    const searchTerm = query.get("q");
+
+    useEffect(()=>{
+        if(searchTerm) {
+            fetchSearchMovie(searchTerm);
+        }
+    },[searchTerm]);
+
+    const fetchSearchMovie = async (searchTerm) => {
+        try{
+            const request = await api.get(
+                `/search/multi?query=${searchTerm}`
+            )
+            console.log(request.data.results);
+            setSearchResult(request.data.results);
+        } catch(error){
+            console.log(error);
+        }
+    }
+
+    const renderSearchResults = () => {
+        return searchResult.length > 0 ? (
+            <section className='search-container'>
+            {searchResult.map((movie) => {
+                if(movie.backdrop_path !== null && movie.media_type !== 'person') {
+                    const movieImageUrl =
+                    "https://image.tmdb.org/t/p/w500" + movie.backdrop_path
+                    return(
+                    <div className='movie' id={movie.id}>
+                            <div className='movie__column-poster'>
+                                <img
+                                    src={movieImageUrl} alt='movie img'
+                                    className='movie__poster'
+                                />
+                                <div className='movie__desc'>
+                                    <p>
+                                        {movie.title}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+            })}
+            </section>
+        ) : (
+            <section className='no-results'>
+                <div className='no-result__text'></div>
+                <p>
+                    찾고자하는 검색어 `{searchTerm}`에 맞는 영화가 없습니다.
+                </p>
+            </section>
+        )
+    }
+
+
+    return renderSearchResults();
 };
 
 export default SearchPage;
