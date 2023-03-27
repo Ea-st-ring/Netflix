3eae37b25452b9ce8e76da04ae1e366fe6fd117e
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2023-03-27 13:33:27 +0900

    feat: 1차 구현

--- netflix/src/pages/DetailPage/index.js
+++ netflix/src/pages/DetailPage/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
-
+import React,{useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import api from '../../api/api';
+import styled from 'styled-components';
 const DetailPage = () => {
+    const {movieId} = useParams();
+    const [movie, setMovie] = useState({});
+    useEffect(() => {
+    async function fetchData(){
+        const request = await api.get(
+            `/movie/${movieId}`
+        )
+        setMovie(request.data);
+    }
+    fetchData();
+    }, [movieId])
+    
+    if(!movie) return <div>...loading</div>
     return (
-        <div>
-            
-        </div>
+        <section>
+            <Poster
+            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+            alt='poster'
+            />
+        </section>
     );
 };
+
+
+const Poster = styled.img`
+    width: 100%;
+    height: auto;
+    margin-bottom: 0;
+`
 
 export default DetailPage;
